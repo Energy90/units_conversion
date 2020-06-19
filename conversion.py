a53093d26b885a4c7048763f568558fa8a50d693
Energy90 <mcmalindi@gmail.com>
2020-06-19 16:27:05 +0200

    heroku deployment

--- conversion.py
+++ conversion.py
@@ -4,7 +4,10 @@ from util import units
 from flask_bootstrap import Bootstrap
 from flask_sijax import Sijax
 from helpers import SijaxHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+
+LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 
@@ -19,7 +22,23 @@ bootstrap = Bootstrap(app)
 sijax = Sijax(app)
 #SECRET_KEY = os.environ.get('SECRET_KEY') or 'let-use-units-conversion-table'
 
-
+if not app.debug and not app.testing:
+   if app.config['LOG_TO_STDOUT']:
+      stream_handler = logging.StreamHandler()
+      stream_handler.setLevel(logging.INFO)
+      app.logger.addHandler(stream_handler)
+   else:
+      if not os.path.exists('logs'):
+         os.mkdir('logs')
+         file_handler = RotatingFileHandler('logs/workon.log', maxBytes=10240, backupCount=10)
+         file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s'
+            '[in %(pathname)s:%(lineno)d]'
+         ))
+         file_handler.setLevel(logging.INFO)
+         app.logger.addHandler(file_handler)
+         app.logger.setLevel(logging.INFO)
+         app.logger.info('units-conversion startup')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
